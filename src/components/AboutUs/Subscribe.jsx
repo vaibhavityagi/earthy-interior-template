4ba10e334ca302681e970762e0581293d32e8e8c
Mukul dubey <mdubey.0433@gmail.com>
2025-11-21 15:46:23 +0530

    change backdrop

--- src/components/AboutUs/Subscribe.jsx
+++ src/components/AboutUs/Subscribe.jsx
@@ -1,7 +1,7 @@
 const Subscribe = () => {
   return (
     <div
-      className="w-[100%] h-[400px] max-md:h-[338px] max-sm:h-[426px] px-[100px] max-md:px-[36px]  justify-center flex items-center "
+      className="relative w-[100%] h-[400px] max-md:h-[338px] max-sm:h-[426px] px-[100px] max-md:px-[36px]  justify-center flex items-center "
       style={{
         backgroundImage: `url("https://69sfgmk1pv2omedb.public.blob.vercel-storage.com/claymist-earthy-interior-template/aboutus/subscription.png")`,
         backgroundSize: "cover",
@@ -9,7 +9,8 @@ const Subscribe = () => {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="flex items-center max-sm:flex-col w-full max-sm:gap-[32px]">
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="relative z-10 flex items-center max-sm:flex-col w-full max-sm:gap-[32px]">
         <div className="font-raleway max-w-[570px] max-md:max-w-[262px] w-full text-[24wwpx]  text-[#FFFFFF] leading-[150%] max-md:text-[16px] max-sm:text-[20px] max-sm:max-w-[349px]">
           <span className="font-bold italic text-[32px]">Join Our Mailing List</span> to Receive updates on new
           textures, project inspirations, and exclusive Claymist insights
